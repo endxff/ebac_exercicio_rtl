4c709d8c27919b2592e75724d92534a076efae75
Endriw <endriwxavier@gmail.com>
2024-06-11 20:46:52 -0300

    teste de inserção de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,25 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react"
+import Post from "."
+import PostComment from "."
+import userEvent from "@testing-library/user-event"
+import exp from "constants"
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+describe("Teste para o componente PostComment", () => {
+	it("Deve renderizar o componente corretamente", () => {
+		render(<PostComment />)
+		expect(screen.getByText("Comentar")).toBeInTheDocument()
+	})
+	it("Deve retornar dois comentários", () => {
+		render(<PostComment />)
+		const textArea = screen.getByTestId("textarea-comentario")
+		const botao = screen.getByTestId("btn-comentar")
+		userEvent.type(textArea, "Primeiro comentário")
+		fireEvent.click(botao)
+
+		userEvent.type(textArea, "Segundo comentário")
+		fireEvent.click(botao)
+
+		expect(screen.getByText("Primeiro comentário")).toBeInTheDocument()
+		expect(screen.getByText("Segundo comentário")).toBeInTheDocument()
+	})
+})
